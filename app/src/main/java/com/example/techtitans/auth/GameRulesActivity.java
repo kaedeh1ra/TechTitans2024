a53b1d8ce6b1868e3c3becc8a4543a95adf391db
Kaedeh1ra <Kaedeh1ra@yandex.ru>
2024-04-16 10:07:05 +0300

    123

--- app/src/main/java/com/example/techtitans/auth/GameRulesActivity.java
+++ app/src/main/java/com/example/techtitans/auth/GameRulesActivity.java
@@ -2,29 +2,32 @@ package com.example.techtitans.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
+import com.example.techtitans.R;
 import com.example.techtitans.content_activities.ConversationActivity;
-import com.example.techtitans.databinding.ActivityGameRulesBinding;
 
 public class GameRulesActivity extends AppCompatActivity {
 
-    private ActivityGameRulesBinding binding;
+    private com.example.techtitans.databinding.ActivityGameRulesBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding = ActivityGameRulesBinding.inflate(getLayoutInflater());
+        binding = com.example.techtitans.databinding.ActivityGameRulesBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        setListeners();
-    }
-
-    private void setListeners() {
-        binding.button.setOnClickListener(v -> {
-            startActivity(new Intent(getApplicationContext(), ConversationActivity.class));
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ConversationActivity.class));
+            }
         });
     }
 }
